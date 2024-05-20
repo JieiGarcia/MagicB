362dfe1ca1d24022dec2bed1154d09d67f6198e0
JieiGarcia <jiei.aca@gmail.com>
2024-05-20 14:24:27 +0900

    first commit but the project is almost finished

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('Magic Eight Ball'),
+            backgroundColor: Colors.blue,
+          ),
+          body: MagicBody(),
+        ),
+      ),
+    ),
+  );
+}
+
+class MagicBody extends StatefulWidget {
+  const MagicBody({super.key});
+
+  @override
+  State<MagicBody> createState() => _MagicBodyState();
+}
+
+class _MagicBodyState extends State<MagicBody> {
+  int ballN = 1;
+
+  void buttonP() {
+    ballN = Random().nextInt(5) + 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        child: Expanded(
+          child: Image.asset('images/ball$ballN.png'),
+        ),
+        onPressed: () {
+          setState(() {
+            buttonP();
+          });
+        },
+      ),
+    );
+  }
+}
